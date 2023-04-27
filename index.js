97a1a45931aeda9306290f1c956b3b56eb2c1595
lucas A.miranda <lucasa.miranda@hotmail.com>
2023-04-27 17:11:54 -0300

    Projeto finalizado com sucesso

---
+++ index.js
@@ -0,0 +1,171 @@
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const moment = require('moment');
+
+const port = 3000;
+const baseURL = `http://localhost:${port}`;
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: '',
+  password: '',
+  database: 'backend'
+});
+
+connection.connect();
+
+
+app.use(express.json());
+
+//Modelo Pessoas
+
+// inserir uma nova pessoa
+app.post('/pessoas', (req, res) => {
+  const pessoa = req.body;
+  const data_cadastro = moment().format('YYYY-MM-DD HH:mm:ss');
+  pessoa.data_cadastro = data_cadastro;
+  connection.query('INSERT INTO pessoas SET ?', pessoa, (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+// atualizar uma pessoa existente
+app.put('/pessoas/:id', (req, res) => {
+  const id = req.params.id;
+  const pessoa = req.body;
+  const data_edicao = moment().format('YYYY-MM-DD HH:mm:ss');
+  pessoa.data_edicao = data_edicao;
+  connection.query('UPDATE pessoas SET ? WHERE id = ?', [pessoa, id], (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+// listar todas as pessoas
+app.get('/pessoas', (req, res) => {
+  connection.query('SELECT * FROM pessoas', (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+// deletar uma pessoa existente
+app.delete('/pessoas/:id', (req, res) => {
+  const id = req.params.id;
+  connection.query('DELETE FROM pessoas WHERE id = ?', id, (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+//Modelo Anotações
+
+// consultar CEP na API externa
+async function consultarCEP(cep) {
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
+  const response = await axios.get(url);
+  return response.data;
+}
+
+// inserir uma nova anotação
+app.post('/anotacoes', async (req, res) => {
+  const { id_pessoa, titulo, descricao, cep } = req.body;
+  try {
+    const endereco = await consultarCEP(cep);
+    if (!endereco.cep) {
+      return res.status(400).json({ message: 'CEP inválido' });
+    }
+    const data_cadastro = moment().format('YYYY-MM-DD HH:mm:ss');
+    await connection.execute(
+      'INSERT INTO anotacoes (id_pessoa, titulo, descricao, cep, endereco, data_cadastro) VALUES (?, ?, ?, ?, ?, ?)',
+      [id_pessoa, titulo, descricao, cep, JSON.stringify(endereco), data_cadastro]
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// listar todas as anotações
+app.get('/anotacoes', async (req, res) => {
+  const [rows] = await connection.execute('SELECT * FROM anotacoes');
+  res.json(rows);
+});
+
+//Testando minha aplicação 
+const axios = require('axios');
+const moment = require('moment');
+
+
+
+describe('Testes para os endpoints', () => {
+
+  test('Deve inserir uma nova pessoa', async () => {
+    const pessoa = {
+      nome: 'Lucas',
+      idade: 30,
+      email: 'lucas@teste.com',
+      cep: '1234-567',
+    };
+    const response = await axios.post(`${baseURL}/pessoas`, pessoa);
+    expect(response.status).toBe(200);
+    expect(response.data).toBeDefined();
+  });
+
+  test('Deve atualizar uma pessoa existente', async () => {
+    const pessoa = {
+      nome: 'Tayse',
+      idade: 43,
+      email: 'tayse@teste.com',
+      cep: '1234-5678',
+    };
+    const responseInsert = await axios.post(`${baseURL}/pessoas`, pessoa);
+    const id = responseInsert.data.insertId;
+    pessoa.nome = 'Fulano da Silva';
+    const responseUpdate = await axios.put(`${baseURL}/pessoas/${id}`, pessoa);
+    expect(responseUpdate.status).toBe(200);
+    expect(responseUpdate.data).toBeDefined();
+  });
+
+  test('Deve listar todas as pessoas', async () => {
+    const response = await axios.get(`${baseURL}/pessoas`);
+    expect(response.status).toBe(200);
+    expect(response.data).toBeDefined();
+  });
+
+  test('Deve deletar uma pessoa existente', async () => {
+    const pessoa = {
+      nome: 'Cida',
+      idade: 61,
+      email: 'cida@teste.com',
+      cep: '1234-56789',
+    };
+    const responseInsert = await axios.post(`${baseURL}/pessoas`, pessoa);
+    const id = responseInsert.data.insertId;
+    const responseDelete = await axios.delete(`${baseURL}/pessoas/${id}`);
+    expect(responseDelete.status).toBe(200);
+    expect(responseDelete.data).toBeDefined();
+  });
+
+  test('Deve inserir uma nova anotação', async () => {
+    const anotacao = {
+      id_pessoa: 1,
+      titulo: 'Teste de anotação',
+      descricao: 'Descrição da anotação',
+      data_cadastro: moment().format('YYYY-MM-DD HH:mm:ss'),
+      data_edicao: moment().format('YYYY-MM-DD HH:mm:ss'),
+    };
+    const response = await axios.post(`${baseURL}/anotacoes`, anotacao);
+    expect(response.status).toBe(201);
+  });
+
+  test('Deve listar todas as anotações', async () => {
+    const response = await axios.get(`${baseURL}/anotacoes`);
+    expect(response.status).toBe(200);
+    expect(response.data).toBeDefined();
+  });
+});
---
+++ pessoas.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27-Abr-2023 às 22:04
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `backend`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoas`
+--
+
+CREATE TABLE `pessoas` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `nome_mae` varchar(255) NOT NULL,
+  `nome_pai` varchar(255) DEFAULT NULL,
+  `cep` varchar(8) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `data_cadastro` date NOT NULL,
+  `data_edicao` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `pessoas`
+--
+ALTER TABLE `pessoas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `pessoas`
+--
+ALTER TABLE `pessoas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
