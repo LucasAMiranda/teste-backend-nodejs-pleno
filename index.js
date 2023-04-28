2214e09206e3505bbb54ea0234e39bef2083ee2c
lucas A.miranda <lucasa.miranda@hotmail.com>
2023-04-28 09:18:31 -0300

    Atualizando o projeto teste backend

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const app = express();
 const axios = require('axios');
 const moment = require('moment');
 const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
+
 
 const port = 3000;
 const baseURL = `http://localhost:${port}`;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: '',
-  password: '',
-  database: 'backend'
+    host: 'localhost', // nome do host,
+    user: 'root',
+    password: '',
+    database: 'backend',
+    port: '3306',
 });
+
 
 connection.connect();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 
@@ -96,80 +101,3 @@ app.get('/anotacoes', async (req, res) => {
   const [rows] = await connection.execute('SELECT * FROM anotacoes');
   res.json(rows);
 });
-
-//Testando minha aplicação 
-const axios = require('axios');
-const moment = require('moment');
-
-
-
-describe('Testes para os endpoints', () => {
-
-  test('Deve inserir uma nova pessoa', async () => {
-    const pessoa = {
-      nome: 'Lucas',
-      idade: 30,
-      email: 'lucas@teste.com',
-      cep: '1234-567',
-    };
-    const response = await axios.post(`${baseURL}/pessoas`, pessoa);
-    expect(response.status).toBe(200);
-    expect(response.data).toBeDefined();
-  });
-
-  test('Deve atualizar uma pessoa existente', async () => {
-    const pessoa = {
-      nome: 'Tayse',
-      idade: 43,
-      email: 'tayse@teste.com',
-      cep: '1234-5678',
-    };
-    const responseInsert = await axios.post(`${baseURL}/pessoas`, pessoa);
-    const id = responseInsert.data.insertId;
-    pessoa.nome = 'Fulano da Silva';
-    const responseUpdate = await axios.put(`${baseURL}/pessoas/${id}`, pessoa);
-    expect(responseUpdate.status).toBe(200);
-    expect(responseUpdate.data).toBeDefined();
-  });
-
-  test('Deve listar todas as pessoas', async () => {
-    const response = await axios.get(`${baseURL}/pessoas`);
-    expect(response.status).toBe(200);
-    expect(response.data).toBeDefined();
-  });
-
-  test('Deve deletar uma pessoa existente', async () => {
-    const pessoa = {
-      nome: 'Cida',
-      idade: 61,
-      email: 'cida@teste.com',
-      cep: '1234-56789',
-    };
-    const responseInsert = await axios.post(`${baseURL}/pessoas`, pessoa);
-    const id = responseInsert.data.insertId;
-    const responseDelete = await axios.delete(`${baseURL}/pessoas/${id}`);
-    expect(responseDelete.status).toBe(200);
-    expect(responseDelete.data).toBeDefined();
-  });
-
-  test('Deve inserir uma nova anotação', async () => {
-    const anotacao = {
-      id_pessoa: 1,
-      titulo: 'Teste de anotação',
-      descricao: 'Descrição da anotação',
-      data_cadastro: moment().format('YYYY-MM-DD HH:mm:ss'),
-      data_edicao: moment().format('YYYY-MM-DD HH:mm:ss'),
-    };
-    const response = await axios.post(`${baseURL}/anotacoes`, anotacao);
-    expect(response.status).toBe(201);
-  });
-
-  test('Deve listar todas as anotações', async () => {
-    const response = await axios.get(`${baseURL}/anotacoes`);
-    expect(response.status).toBe(200);
-    expect(response.data).toBeDefined();
-  });
-});
-
-// Configuração do Swagger UI...
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
