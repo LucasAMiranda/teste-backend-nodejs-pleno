881a01898b6cd8247e3052fbeb2cb4c1e851b56c
lucas A.miranda <lucasa.miranda@hotmail.com>
2023-04-30 20:29:44 -0300

    Projeto atualizado com sucessoe concluido

--- AnotacoesService.js
+++ AnotacoesService.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const moment = require('moment');
-const { verifyToken } = require('./authMiddleware');
+const { verifyToken } = require('./env');
 const connection = require('./db');
 
 // consultar CEP na API externa
